685b8a92c72a459a300044c706c71fc92d094827
sutin1234 <tony.sutin1234@gmail.com>
2021-01-26 11:40:21 +0700

    added to bigin

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,43 +6,8 @@
 	<h1>Hello world!</h1>
 
 	<Counter/>
-	<p>Visit the <a class="text-red-600 underline" href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+	<p>Visit the <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
-	:root {
-		@apply font-sans;
-	}
-
-	main {
-		@apply text-center;
-		@apply p-4;
-		@apply mx-auto;
-	}
-
-	h1 {
-		@apply text-red-600;
-		@apply uppercase;
-		@apply text-6xl;
-		@apply font-thin;
-		@apply leading-tight;
-		@apply my-16 mx-auto;
-		@apply max-w-xs;
-	}
-
-	p {
-		@apply max-w-xs;
-		@apply my-8 mx-auto;
-		@apply leading-snug;
-	}
-
-	@screen sm {
-		h1 {
-			@apply max-w-none;
-		}
-
-		p {
-			@apply max-w-none;
-		}
-	}
 </style>

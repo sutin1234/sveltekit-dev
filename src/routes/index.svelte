38fa0db1652a4708139b775707187567a715db13
sutin1234 <tony.sutin1234@gmail.com>
2021-01-27 10:10:34 +0700

    added example tailkwindcss

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,51 @@
 <script>
-	import Counter from '@/components/Counter.svelte';
+  import Counter from "@/components/Counter.svelte";
 </script>
 
 <main>
-	<h1>Hello world!</h1>
+  <h1>Hello world!</h1>
 
-	<Counter/>
-	<p>Visit the <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.</p>
+  <Counter />
+  <p>
+    Visit the <a class="bg-red-300" href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+    apps.
+  </p>
 </main>
 
 <style>
+  :root {
+    @apply font-sans;
+  }
+
+  main {
+    @apply text-center;
+    @apply p-4;
+    @apply mx-auto;
+  }
+
+  h1 {
+    @apply text-red-600;
+    @apply uppercase;
+    @apply text-6xl;
+    @apply font-thin;
+    @apply leading-tight;
+    @apply my-16 mx-auto;
+    @apply max-w-xs;
+  }
+
+  p {
+    @apply max-w-xs;
+    @apply my-8 mx-auto;
+    @apply leading-snug;
+  }
+
+  @screen sm {
+    h1 {
+      @apply max-w-none;
+    }
+
+    p {
+      @apply max-w-none;
+    }
+  }
 </style>
